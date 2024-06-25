a12b2846e4e6ea264fb822d4085edd2fae422349
Piotr <dsw48701@student.dsw.edu.pl>
2024-06-25 20:45:49 +0200

    4 team commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,44 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from .models import Transaction, Categories
+import plotly.express as px
+from django.db.models import Sum
+import pandas as pd
 
 
 @login_required
 def dashboard(request):
-    if (
-        hasattr(request.user, "userprofile")
-        and request.user.userprofile.must_change_password
-    ):
-        return redirect("change_password")
-    return render(request, "dashboard/dashboard.html", {"user": request.user})
+    transactions = Transaction.objects.filter(user=request.user).order_by("-date")
+    categories = Categories.objects.all()
+
+    category_sums = (
+        Transaction.objects.filter(user=request.user)
+        .values("category__name")
+        .annotate(total_amount=Sum("amount"))
+    )
+
+    category_sums_list = list(category_sums)
+    if category_sums_list:
+        df = pd.DataFrame(category_sums_list)
+        df.columns = [
+            "Category",
+            "Total Amount",
+        ]
+
+        fig = px.bar(
+            df,
+            x="Category",
+            y="Total Amount",
+            labels={"Category": "Category", "Total Amount": "Total Amount"},
+        )
+        plot_div = fig.to_html(full_html=False)
+    else:
+        plot_div = "<p>No data available to display the chart.</p>"
+
+    context = {
+        "transactions": transactions,
+        "categories": categories,
+        "plot_div": plot_div,
+    }
+
+    return render(request, "dashboard/dashboard.html", context)
