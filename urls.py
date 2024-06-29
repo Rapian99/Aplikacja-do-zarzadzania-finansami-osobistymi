6b00e80e8c3dd92bc248e285d972c2f8912bb45e
Wojciech <69200642+wpitucha@users.noreply.github.com>
2024-06-29 18:55:10 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('create/', views.create_transaction, name='create_transaction'),
-]
