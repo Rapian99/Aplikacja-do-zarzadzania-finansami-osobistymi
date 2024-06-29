da64df3d30d9b356a70c6c2966094b1a194080cf
Wojciech <69200642+wpitucha@users.noreply.github.com>
2024-06-29 18:43:08 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    symbol = models.CharField(max_length=10, unique=True)
+    exchange_rate = models.DecimalField(max_digits=20, decimal_places=6)
+
+    def __str__(self):
+        return self.name
+
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.name}"
+
+
+class UserCurrencyData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.currency.name}"
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    type = models.CharField(
+        max_length=20, choices=[("deposit", "Deposit"), ("withdrawal", "Withdrawal")]
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    transaction_type = models.CharField(
+        max_length=10, choices=[("deposit", "Deposit"), ("withdrawal", "Withdrawal")]
+    )
+    date = models.DateField()
+    description = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.transaction_type} - {self.amount} {self.currency.symbol} on {self.date}"
+    
+class WishlistItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=255)
+    item_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item_name
